5717bdcaffdb58ed56362727b3fd81330e3c5012
msaaddev <mrsaadirfan@gmail.com>
2020-10-09 20:09:22 +0500

    📦 NEW: header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <header className="jumbotron">
+        <div className="container">
+            <div className="row row-header">
+                <div className="col-6 col-sm-6">
+                    <h1>Notes <span className="badge badge-secondary">v0.0.1</span></h1>
+                </div>
+                <div className="col-6 col-sm-6">
+
+                </div>
+            </div>
+          </div>
+        </header>
+
     </div>
   );
 }
