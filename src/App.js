4a742ea7875b6f57b3c5c246c34e20379c4d2ca0
msaaddev <mrsaadirfan@gmail.com>
2020-10-09 20:56:27 +0500

    👌 IMPROVE: indentation

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-        <header className="jumbotron">
-        <div className="container">
-            <div className="row row-header">
-                <div className="col-6 col-sm-6">
-                    <h1>Notes <span className="badge badge-secondary">v0.0.1</span></h1>
+    return (
+        <div>
+            <header className='jumbotron'>
+                <div className='container'>
+                    <div className='row row-header'>
+                        <div className='col-6 col-sm-6'>
+                            <h1>
+                                Notes <span className='badge badge-secondary'>v0.0.1</span>
+                            </h1>
+                        </div>
+                        <div className='col-6 col-sm-6'></div>
+                    </div>
                 </div>
-                <div className="col-6 col-sm-6">
-                </div>
-            </div>
-          </div>
-        </header>
-
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
