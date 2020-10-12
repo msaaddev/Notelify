2e1ae4247e2c2669104fb53af4caedd83d9e3210
msaaddev <mrsaadirfan@gmail.com>
2020-10-12 19:29:01 +0500

    📦 NEW: data points

---
+++ src/data/notes.js
@@ -0,0 +1,48 @@
+module.exports = {
+    note1: [
+        {
+            badgeType: 'badge-warning',
+            badgeContent: 'Deadline',
+            noteContent: 'Calculus homework',
+        },
+        {
+            badgeType: 'badge-light',
+            badgeContent: '16-30',
+            noteContent: 'Meet Alice at Hard Rock Cafe',
+        },
+    ],
+    note2: [
+        {
+            badgeType: 'badge-warning',
+            badgeContent: 'Deadline',
+            noteContent: 'Java test on Maven',
+        },
+        {
+            badgeType: 'badge-light',
+            badgeContent: '15-00',
+            noteContent: 'Gym',
+        },
+        {
+            badgeType: 'badge-info',
+            badgeContent: 'Links or references',
+            noteContent: "The hitchhiker's guide to the galaxy",
+        },
+    ],
+    note3: [
+        {
+            badgeType: 'badge-warning',
+            badgeContent: 'Deadline',
+            noteContent: 'Economics homework',
+        },
+        {
+            badgeType: 'badge-light',
+            badgeContent: '18-00',
+            noteContent: 'Call my team to discuss project',
+        },
+        {
+            badgeType: 'badge-info',
+            badgeContent: 'Links or references',
+            noteContent: 'Introduction to Algorithms, CLRS',
+        },
+    ],
+};
---
+++ src/data/notesHead.js
@@ -0,0 +1,20 @@
+module.exports = {
+    note1: {
+        monthDay: '7th October',
+        weekDay: 'Wednesday',
+        relativeDay: 'Yesterday',
+        badgeType: 'badge-danger',
+    },
+    note2: {
+        monthDay: '8th October',
+        weekDay: 'Thursday',
+        relativeDay: 'Today',
+        badgeType: 'badge-warning',
+    },
+    note3: {
+        monthDay: '9th October',
+        weekDay: 'Friday',
+        relativeDay: 'Tomorrow',
+        badgeType: 'badge-primary',
+    },
+};
